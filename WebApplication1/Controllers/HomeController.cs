020873483df4cabd52d0a3dec903e49ac4681c60
Fatumner <Fatumner@wp.pl>
2014-06-30 22:31:06 +0200

    trying to fix "no parameterless public constructor in HomeController"

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private CountryAndLanguageModel.CountryAndLanguageEntities db = new CountryAndLanguageModel.CountryAndLanguageEntities();
-
+        public HomeController() { }
         // GET: Home
         public ActionResult Index()
         {
